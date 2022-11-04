0fbc41c642af5e94ca1efcd874882fa775d1a0c4
Rachel-T-Casey <General@Rachel-Casey.com>
2022-11-04 14:15:01 -0600

    front end sends data to back end

--- client/src/pages/Register.js
+++ client/src/pages/Register.js
@@ -1,44 +1,43 @@
 import React from 'react'
+import { useState } from 'react';
+// import axios 
+import axios from 'axios';
 
 // A function to send a POST request to the server
 
-function registerUser() {
-  // Create a new FormData object
-  const formData = new FormData();
-  // Append the username, email, and password to the formData object
-  formData.append('username', document.querySelector('input[name="username"]').value);
-  formData.append('email', document.querySelector('input[name="email"]').value);
-  formData.append('password', document.querySelector('input[name="password"]').value);
-  // Send a post request to the backend
-  fetch('http://localhost:8081/api/users/register', {
-    method: 'POST',
-    body: formData,
-  })
-    // If the response is ok, redirect to the bugtracker page
-    .then((res) => {
-      if (res.ok) {
-        window.location.href = '/bugtracker';
-      }
-    })
-    // If there's an error, log the error to the console
-    .catch((err) => console.log(err));
-}
-function register() {
+
+function Register() {
+  const [username, setUsername] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+    const registerUser = async (e) => {
+    e.preventDefault();
+      const response = await axios.post('http://localhost:5000/api/users/register', {
+      username: {username},
+      email: {email},
+      password: {password}
+    });
+    console.log(response);
+  }
+
+
+
   return (
     /* A form to register a new user, with a username, email and password field and a submit button */
     <div>
       <h1>Register</h1>
       <form onSubmit={registerUser}>
         <label>Username</label>
-        <input type="text" name="username" />
+        <input type="text" name="username" onChange={(e) => setUsername(e.target.value)} />
         <label>Email</label>
-        <input type="email" name="email" />
+        <input type="email" name="email" onChange={(e) => setEmail(e.target.value)} />
         <label>Password</label>
-        <input type="password" name="password" />
+        <input type="password" name="password" onChange={(e) => setPassword(e.target.value)} />
         <button type="submit">Submit</button>
       </form>
     </div>
      )
 }
 
-export default register
+export default Register
